1ead5040dd091652a87a9428c72e426dc029bd41
lanvce <1569101820@qq.com>
2022-02-14 21:54:59 +0800

    修改脚本

--- startup.sh
+++ startup.sh
@@ -3,8 +3,8 @@ export JAVA_HOME PATH CLASSPATH
 JAVA_HOME=/opt/jdk1.8.0_311/
 PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 CLASSPATH=.:$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CLASSPATH
-DIR=/opt/apps/jenkins/hydd-erp
-JARFILE=jsh-boot.jar
+DIR=/opt/apps/jenkins/hydd-boot
+JARFILE=jshERP.jar
 
 if [ ! -d $DIR/backup ];then
    mkdir -p $DIR/backup
@@ -13,7 +13,7 @@ cd $DIR
 
 ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}' | xargs kill -9
 mv $JARFILE backup/$JARFILE$DATE
-mv -f /root/.jenkins/workspace/Cloud-Amazon-Admin/amazon-admin/target/$JARFILE .
+mv -f /var/lib/jenkins/workspace/hydd-boot/target/$JARFILE .
 
 java -jar $JARFILE --spring.profiles.active=prod  > out.log &
 if [ $? = 0 ];then
