6af8eefe9a345f0c6bb084189c8b28eca36f75c3
lanvce <1569101820@qq.com>
2022-02-14 21:11:08 +0800

    2-14修改

---
+++ startup.sh
@@ -0,0 +1,26 @@
+DATE=$(date +%Y%m%d)
+export JAVA_HOME PATH CLASSPATH
+JAVA_HOME=/opt/jdk1.8.0_311/
+PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
+CLASSPATH=.:$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CLASSPATH
+DIR=/opt/apps/jenkins/hydd-erp
+JARFILE=jsh-boot.jar
+
+if [ ! -d $DIR/backup ];then
+   mkdir -p $DIR/backup
+fi
+cd $DIR
+
+ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}' | xargs kill -9
+mv $JARFILE backup/$JARFILE$DATE
+mv -f /root/.jenkins/workspace/Cloud-Amazon-Admin/amazon-admin/target/$JARFILE .
+
+java -jar $JARFILE --spring.profiles.active=prod  > out.log &
+if [ $? = 0 ];then
+        sleep 30
+        tail -n 50 out.log
+fi
+
+cd backup/
+ls -lt|awk 'NR>5{print $NF}'|xargs rm -rf
+
